ddf7374de91e2e8f2b3681f5b55208852cac2035
vxiisack.exe <lesenokcesir@gmail.com>
2026-06-16 20:50:02 +0700

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,15 +1,1 @@
-f = open("input.txt", "r", encoding="utf-8")
-lines = f.readlines()
-f.close()
 
-line_count = len(lines)
-word_count = 0
-
-for line in lines:
-    words = line.strip().split()
-    word_count += len(words)
-
-f = open("statistics.txt", "w", encoding="utf-8")
-f.write("Строк: " + str(line_count) + "\n")
-f.write("Слов: " + str(word_count))
-f.close()
