4c1196bb4bda55ef70ab03b5c2938605ef55e6c2
vxiisack.exe <lesenokcesir@gmail.com>
2026-06-16 20:50:49 +0700

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -1,18 +1,1 @@
-f = open("words.txt", "r", encoding="utf-8")
-words = f.read().splitlines()
-f.close()
 
-sorted_alpha = sorted(words)
-f = open("sorted_alphabetically.txt", "w", encoding="utf-8")
-f.write("\n".join(sorted_alpha))
-f.close()
-
-sorted_len = sorted(words, key=len)
-f = open("sorted_by_length.txt", "w", encoding="utf-8")
-f.write("\n".join(sorted_len))
-f.close()
-
-sorted_rev = sorted(words, reverse=True)
-f = open("sorted_reverse.txt", "w", encoding="utf-8")
-f.write("\n".join(sorted_rev))
-f.close()
